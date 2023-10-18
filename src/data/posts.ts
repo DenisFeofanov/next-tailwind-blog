10675d3fe5a72b5d0eb93f6d19e741e551fca512
DenisFeofanov <omail4me@gmail.com>
2023-10-18 14:17:39 +0500

    add more hard-coded posts

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -4,11 +4,29 @@ interface Post {
   text: string;
   author_avatar: string;
   author_name: string;
+  date: string;
 }
 
 export const posts: Post[] = [
   {
     cover: "/post-image.png",
+    date: "March 16, 2020",
+    heading: "Dynamic Routing and Static Generation",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididuntut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est    ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus",
+    author_avatar: "/user-logo.png",
+    author_name: "JJ Kasper",
+  },
+  {
+    cover: "/post-image.png",
+    date: "March 16, 2020",
+    heading: "Dynamic Routing and Static Generation",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididuntut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est    ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus",
+    author_avatar: "/user-logo.png",
+    author_name: "JJ Kasper",
+  },
+  {
+    cover: "/post-image.png",
+    date: "March 16, 2020",
     heading: "Dynamic Routing and Static Generation",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididuntut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est    ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus",
     author_avatar: "/user-logo.png",
