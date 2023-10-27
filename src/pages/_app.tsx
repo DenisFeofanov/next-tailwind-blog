6959aa73f6bd20a9cb2503e6a1f49c0266cbc7f9
DenisFeofanov <omail4me@gmail.com>
2023-10-27 17:44:16 +0500

    add loading bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Montserrat } from "next/font/google";
+import NextNProgress from "nextjs-progressbar";
 
 const montserrat = Montserrat({
   weight: ["400", "700"],
@@ -10,6 +11,8 @@ const montserrat = Montserrat({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={montserrat.className}>
+      <NextNProgress stopDelayMs={0} />
+
       <Component {...pageProps} />
     </div>
   );
