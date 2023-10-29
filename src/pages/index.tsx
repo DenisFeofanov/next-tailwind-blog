b66dfe0a191a8db89f9afca761e8d18358950268
DenisFeofanov <omail4me@gmail.com>
2023-10-29 13:53:04 +0500

    feat: add webpage title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,16 @@ import MorePosts from "@/components/MorePosts";
 import PreviewPost from "@/components/PreviewPost";
 import { Post } from "@/interfaces/Post";
 import { getAllPostsForHome } from "@/lib/api";
+import Head from "next/head";
 
 export default function Home({ allPosts }: { allPosts: Post[] }) {
   const previewPost = allPosts[0];
   const morePosts = allPosts.slice(1, 3);
   return (
     <Layout home>
+      <Head>
+        <title>Cat&apos;s life</title>
+      </Head>
       {allPosts.length > 0 ? (
         <>
           <PreviewPost post={previewPost} />
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -6,6 +6,7 @@ import Text from "@/components/Text";
 import { Post } from "@/interfaces/Post";
 import { getPostBySlug, getPostsSlugs } from "@/lib/api";
 import type { GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
 import { ParsedUrlQuery } from "querystring";
 
 interface Params extends ParsedUrlQuery {
@@ -20,26 +21,27 @@ export default function Post({
   post: { coverImage, title, excerpt, author, date },
 }: Props) {
   return (
-    <>
-      <Layout>
-        <h1 className="text-black text-8xl font-bold leading-tight tracking-[-0.25rem]">
-          {title}
-        </h1>
+    <Layout>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <h1 className="text-black text-8xl font-bold leading-tight tracking-[-0.25rem]">
+        {title}
+      </h1>
 
-        <Author className="mt-3" author={author} />
+      <Author className="mt-3" author={author} />
 
-        <CoverImage
-          className="mt-12"
-          title={title}
-          responsiveImage={coverImage.responsiveImage}
-        />
+      <CoverImage
+        className="mt-12"
+        title={title}
+        responsiveImage={coverImage.responsiveImage}
+      />
 
-        <article className="w-[45%] mx-auto">
-          <Date className="mt-16">{date}</Date>
-          <Text className="mt-6">{excerpt}</Text>
-        </article>
-      </Layout>
-    </>
+      <article className="w-[45%] mx-auto">
+        <Date className="mt-16">{date}</Date>
+        <Text className="mt-6">{excerpt}</Text>
+      </article>
+    </Layout>
   );
 }
 
